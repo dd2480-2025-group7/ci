df6eec38fc1222680ce1bf79e08584f1c34fc39c
Vilhelm Prytz <vilhelm@prytznet.se>
2025-02-05 09:57:59 +0100

    feat: implement basic Maven project
    fixes #10

--- ciapp/src/main/java/com/gorup7/ciapp/ContinuousIntegrationServer.java
+++ ciapp/src/main/java/com/gorup7/ciapp/ContinuousIntegrationServer.java
@@ -1,25 +1,24 @@
+package com.group7.ciapp;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.ServletException;
- 
+
 import java.io.IOException;
- 
-import org.eclipse.jetty.server.Server;
+
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.handler.AbstractHandler;
 
-/** 
- Skeleton of a ContinuousIntegrationServer which acts as webhook
- See the Jetty documentation for API documentation of those classes.
-*/
-public class ContinuousIntegrationServer extends AbstractHandler
-{
+/**
+ * Skeleton of a ContinuousIntegrationServer which acts as webhook
+ * See the Jetty documentation for API documentation of those classes.
+ */
+public class ContinuousIntegrationServer extends AbstractHandler {
     public void handle(String target,
-                       Request baseRequest,
-                       HttpServletRequest request,
-                       HttpServletResponse response) 
-        throws IOException, ServletException
-    {
+            Request baseRequest,
+            HttpServletRequest request,
+            HttpServletResponse response)
+            throws IOException, ServletException {
         response.setContentType("text/html;charset=utf-8");
         response.setStatus(HttpServletResponse.SC_OK);
         baseRequest.setHandled(true);
@@ -33,13 +32,13 @@ public class ContinuousIntegrationServer extends AbstractHandler
 
         response.getWriter().println("CI job done");
     }
- 
+
     // used to start the CI server in command line
-    public static void main(String[] args) throws Exception
-    {
-        Server server = new Server(8080);
-        server.setHandler(new ContinuousIntegrationServer()); 
-        server.start();
-        server.join();
-    }
+    // public static void main(String[] args) throws Exception
+    // {
+    // Server server = new Server(8080);
+    // server.setHandler(new ContinuousIntegrationServer());
+    // server.start();
+    // server.join();
+    // }
 }
