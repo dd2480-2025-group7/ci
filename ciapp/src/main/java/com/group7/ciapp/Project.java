978352e023a0c8a6f95b15f95a3defbbc91a63a1
Vilhelm Prytz <vilhelm@prytznet.se>
2025-02-06 15:32:44 +0100

    feat: initial project structure and some work on methods (#18)
    * feat: initial project structure and some work on methods
    
    * initial project structure, fixes #3
    * write Project.java, fixes #14
    * write WebServer.java, fixes #16
    * create StoreBuildResult.java, fixes #17
    
    Co-authored-by: Philip Ågren Jahnsson <philipaj-kth@users.noreply.github.com>
    Co-authored-by: Elin Fransholm <efransholm@users.noreply.github.com>
    
    * fix: remove comments
    
    ---------
    
    Co-authored-by: Philip Ågren Jahnsson <philipaj-kth@users.noreply.github.com>
    Co-authored-by: Elin Fransholm <efransholm@users.noreply.github.com>

---
+++ ciapp/src/main/java/com/group7/ciapp/Project.java
@@ -0,0 +1,74 @@
+package com.group7.ciapp;
+
+import org.eclipse.jgit.api.Git;
+import java.io.File;
+
+public class Project {
+    private String url;
+    private String commit;
+    private Git git;
+
+    public Project(String url, String commit) {
+        this.url = url;
+        this.commit = commit;
+    }
+
+    // create main method that uses JGit to clone the repository
+    public static void main(String url, String commit) {
+    }
+
+    public boolean start() {
+        System.out.println("Repo URL: " + url);
+        System.out.println("Commit: " + commit);
+
+        String path = System.getProperty("user.home") + "/cirepo"; // clone repo inside users home directory
+
+        System.out.println("Cloning repository to: " + path);
+        try {
+            git = Git.cloneRepository().setURI(url).setDirectory(new File(path)).call();
+            // checkout specific commit
+            // "git checkout < commit >"
+            git.checkout().setName(commit).call();
+
+            git.close();
+        } catch (Exception e) {
+            System.out.println("Error cloning repository");
+            // print exception code
+            System.out.println(e);
+            return false;
+        }
+
+        // run tests for specified project after cloning
+        // return boolean based on test results
+        boolean result = runMavenTests(path);
+
+        // delete the cloned repository, forecfully the whole directory using java
+
+        // if tests pass, return true
+        // if tests fail, return false
+        return result;
+    }
+
+    private boolean runMavenTests(String path) {
+        int exitcode = -1;
+        try {
+            ProcessBuilder builder = new ProcessBuilder("mvn", "package");
+            builder.directory(new File(path + "/ciapp"));
+            builder.redirectErrorStream(true); // merges stdout and stderr
+            Process process = builder.start();
+
+            exitcode = process.waitFor(); // wait for process to finish running tests
+
+        } catch (Exception e) {
+
+        }
+        if (exitcode == 0) {
+            // DEBUG
+            System.out.println("Tests passed");
+            return true;
+        } else {
+            System.out.println("Tests did not pass");
+            return false;
+        }
+    }
+}
---
+++ ciapp/src/main/java/com/group7/ciapp/StoreBuildResult.java
@@ -0,0 +1,7 @@
+package com.group7.ciapp;
+
+public class StoreBuildResult {
+    // post request to GitHub API to flag commit as success or failure
+
+    // store build logs in a file
+}
