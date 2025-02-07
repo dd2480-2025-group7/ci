57b6db79b191ebc596867a303d5ece8d364524b2
lindanycander <76840066+lindanycander@users.noreply.github.com>
2025-02-07 13:49:12 +0100

    feat: add javadoc comments to App.java

--- ciapp/src/main/java/com/group7/ciapp/App.java
+++ ciapp/src/main/java/com/group7/ciapp/App.java
@@ -1,12 +1,21 @@
 package com.group7.ciapp;
 
 import org.eclipse.jetty.server.Server;
-import com.group7.ciapp.WebServer;
 
 /**
- * Hello world!
+ * App
+ * 
+ * This class is responsible for starting the server and listening for incoming
+ * requests.
  */
 public class App {
+
+    /**
+     * Starts the server and listens for incoming requests
+     * 
+     * @param args
+     * @throws Exception
+     */
     public static void main(String[] args) throws Exception {
         Server server = new Server(8080);
         server.setHandler(new WebServer());
