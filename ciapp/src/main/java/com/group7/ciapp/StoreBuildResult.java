b628c9a9a7c95cb72d1f518a9cce01c87a36c2ec
Vilhelm Prytz <vilhelm@prytznet.se>
2025-02-07 09:39:41 +0100

    feat: cleanup and methods in StoreBuildResult.java (#20)
    * feat: cleanup and methods in StoreBuildResult.java (WIP)
    
    * started work on issue #15 (WIP)
    * minor cleanup and adjustments
    
    Co-authored-by: Philip Ågren Jahnsson <philipaj-kth@users.noreply.github.com>
    Co-authored-by: linda.nycander@gmail.com <linda.nycander@gmail.com>
    Co-authored-by: Elin Fransholm <efransholm@users.noreply.github.com>
    
    * feat: change setStatusCompelte from post to patch request
    
    Get checkrun-id from setStatusBuilding and use it for the patch-request in setStatusComplete
    
    closes #15
    
    Co-authored-by: Philip Ågren Jahnsson <philipaj-kth@users.noreply.github.com>
    Co-authored-by: Elin Fransholm <efransholm@users.noreply.github.com>
    Co-authored-by: Vilhelm Prytz <vilhelm@prytznet.se>
    
    ---------
    
    Co-authored-by: Philip Ågren Jahnsson <philipaj-kth@users.noreply.github.com>
    Co-authored-by: linda.nycander@gmail.com <linda.nycander@gmail.com>
    Co-authored-by: Elin Fransholm <efransholm@users.noreply.github.com>
    Co-authored-by: lindanycander <76840066+lindanycander@users.noreply.github.com>

--- ciapp/src/main/java/com/group7/ciapp/StoreBuildResult.java
+++ ciapp/src/main/java/com/group7/ciapp/StoreBuildResult.java
@@ -1,7 +1,103 @@
 package com.group7.ciapp;
 
-public class StoreBuildResult {
-    // post request to GitHub API to flag commit as success or failure
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse;
 
+import java.io.IOException;
+import java.text.ParseException;
+
+import org.apache.hc.client5.http.classic.methods.HttpPost;
+import org.apache.hc.client5.http.classic.methods.HttpPatch;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.core5.http.HttpHeaders;
+import org.apache.hc.core5.http.io.entity.EntityUtils;
+import org.apache.hc.core5.http.io.entity.StringEntity;
+import org.apache.hc.core5.http.ContentType;
+import org.json.JSONObject;
+
+public class StoreBuildResult {
+    private String github_token;
+
+    public StoreBuildResult() {
+        // Get the environment variable
+        this.github_token = System.getenv("GITHUB_ACCESS_TOKEN");
+
+        // Check if the variable is set
+        if (this.github_token == null) {
+            // very much error
+        }
+    }
+
+    /**
+     * post request to GitHub API to flag commit as success or failure
+     * 
+     * @param commitHash (String) The hash of the commit being tested
+     * @param owner      (String) The owner of the repo.
+     * @param repo       (boolean) The GitHub repo that is being used.
+     * @return
+     * @throws IOException
+     * @throws org.apache.hc.core5.http.ParseException
+     */
+    public int setStatusBuilding(String commitHash, String owner, String repo)
+            throws IOException, ParseException, org.apache.hc.core5.http.ParseException {
+        CloseableHttpClient client = HttpClients.createDefault();
+        HttpPost request = new HttpPost(String.format("https://api.github.com/repos/%s/%s/check-runs", owner, repo));
+        request.setHeader(HttpHeaders.ACCEPT, "application/vnd.github.v3+json");
+        request.addHeader(HttpHeaders.AUTHORIZATION, "Bearer " + this.github_token);
+        request.addHeader(HttpHeaders.CONTENT_TYPE, "application/json");
+        request.addHeader(HttpHeaders.USER_AGENT, "Apache HttpClient");
+        String jsonBody = "{"
+                + "\"name\": \"CI Build\","
+                + "\"head_sha\": \"" + commitHash + "\","
+                + "\"status\": \"in_progress\","
+                + "\"output\": {"
+                + "   \"title\": \"Testing\","
+                + "   \"summary\": \"The build is currently being tested.\""
+                + "}"
+                + "}";
+        request.setEntity(new StringEntity(jsonBody, ContentType.APPLICATION_JSON));
+        CloseableHttpResponse response = client.execute(request);
+
+        // Get JSON response as org.JSON.JSONObject
+        String responseString = EntityUtils.toString(response.getEntity());
+        JSONObject jsonResponse = new org.json.JSONObject(responseString);
+        int check_id = jsonResponse.getInt("id");
+        return check_id;
+    }
+
+    /**
+     * Sends a HTTP POST request containing the status of the build and tests.
+     * If successful, set "conclusion" to success. Otherwise, set "conclusion" to
+     * failure.
+     * 
+     * @param commitHash (String) The hash of the commit being tested
+     * @param owner      (String) The owner of the repo.
+     * @param repo       (boolean) The GitHub repo that is being used.
+     * @param isSuccess  (boolean) Says whether the tests passsed or not.
+     * @return
+     * @throws IOException
+     */
+    public void setStatusComplete(String commitHash, String owner, String repo, boolean isSuccess, int check_id)
+            throws IOException {
+        CloseableHttpClient client = HttpClients.createDefault();
+        HttpPatch request = new HttpPatch(
+                String.format("https://api.github.com/repos/%s/%s/check-runs/%d", owner, repo, check_id));
+        request.setHeader(HttpHeaders.ACCEPT, "application/vnd.github.v3+json");
+        request.setHeader(HttpHeaders.AUTHORIZATION, "Bearer " + this.github_token);
+        request.setHeader(HttpHeaders.CONTENT_TYPE, "application/json");
+        String jsonBody = "{"
+                + "\"status\": \"completed\","
+                + "\"conclusion\": \"" + isSuccess + "\","
+                + "\"output\": {"
+                + "   \"title\": \"Build Passed\","
+                + "   \"summary\": \"The build and tests passed successfully.\""
+                + "}"
+                + "}";
+        request.setEntity(new StringEntity(jsonBody, ContentType.APPLICATION_JSON));
+        CloseableHttpResponse response = client.execute(request);
+
+        // print response data in JSON
+        System.out.println(response);
+    }
     // store build logs in a file
 }
