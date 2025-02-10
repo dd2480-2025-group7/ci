cc651deb195a2a2b7d7a89e19d2ad0e378c9faae
Vilhelm Prytz <vilhelm@prytznet.se>
2025-02-10 08:45:42 +0100

    bug: exit the program if required environment variables are not set
    exit the program in App.java before webserver starts if the env
    variables are not set upon runtime
    
    fixes #54

--- ciapp/src/main/java/com/group7/ciapp/App.java
+++ ciapp/src/main/java/com/group7/ciapp/App.java
@@ -15,6 +15,13 @@ public class App {
      * @throws Exception
      */
     public static void main(String[] args) throws Exception {
+        // check that required environment variables are set
+        // PRIVATE_KEY_PATH and APP_ID
+        if (System.getenv("PRIVATE_KEY_PATH") == null || System.getenv("APP_ID") == null) {
+            System.out.println("Please set the environment variables PRIVATE_KEY_PATH and APP_ID");
+            System.exit(1);
+        }
+
         Server server = new Server(8080);
         server.setHandler(new WebServer());
         server.start();
